19f0f1e0196581483dde5985971c2e607667e921
Patryck Silva <patrycksilva70@gmail.com>
2022-05-03 10:08:04 -0300

    Adicionando pasta do projeto spring, pasta back end -> git clone

---
+++ src/main/java/com/generation/graphome/GraphomeApplication.java
@@ -0,0 +1,13 @@
+package com.generation.graphome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/graphome/GraphomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.graphome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
