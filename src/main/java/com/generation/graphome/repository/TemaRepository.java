4804ca0faa4ba0d3ea6656471ce96bfd38be515f
Patryck Silva <patrycksilva70@gmail.com>
2022-05-06 10:48:21 -0300

    Adicionando as classes repository e controller

---
+++ src/main/java/com/generation/graphome/repository/TemaRepository.java
@@ -0,0 +1,16 @@
+package com.generation.graphome.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.graphome.model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository <Tema,Long>{
+
+	public List<Tema> findByNomeContainingIgnoreCase(@Param("nome")String nome);
+	
+}
