ec2397aa37f602ab073d83e4db79e5ba9823fa73
Patryck Silva <patrycksilva70@gmail.com>
2022-05-06 10:42:17 -0300

    Na classe model mudando o atributo resumo para nome

--- src/main/java/com/generation/graphome/model/Tema.java
+++ src/main/java/com/generation/graphome/model/Tema.java
@@ -19,7 +19,7 @@ private Long id;
 
 @NotBlank ( message = "O título do tema é obrigatório")
 @Size( min = 5, max = 100 ,message = " O texto deve ter no mínimo 5 e no máximo 100" )
-private String resumo;
+private String nome;
 
 
 @NotBlank ( message = "A descrição é obrigatória!")
@@ -37,13 +37,13 @@ public void setId(Long id) {
 }
 
 
-public String getResumo() {
-	return resumo;
+public String getNome() {
+	return nome;
 }
 
 
-public void setResumo(String resumo) {
-	this.resumo = resumo;
+public void setNome(String nome) {
+	this.nome = nome;
 }
 
 
