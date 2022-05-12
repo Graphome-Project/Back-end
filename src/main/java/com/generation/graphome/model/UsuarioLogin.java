aea625363b359a8b29699d7088e97652330d971a
Viviane <vivi29emi@gmail.com>
2022-05-12 11:00:22 -0300

    Alterado o atributo user para usuario

--- src/main/java/com/generation/graphome/model/UsuarioLogin.java
+++ src/main/java/com/generation/graphome/model/UsuarioLogin.java
@@ -1,5 +1,93 @@
 package com.generation.graphome.model;
 
-public class UsuarioLogin {
 
+
+public class UsuarioLogin {
+	
+private Long id;
+	
+	
+	private String nome;
+	
+	
+	private String user;
+	
+	
+	private String senha;
+	
+	
+	private String foto;
+	
+	private String bio;
+	
+	private String tipo;
+	
+	private String token;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+	
 }
