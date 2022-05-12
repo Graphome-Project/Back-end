ddbda16d3e31167f7c523496a5470fe6c4e1ae12
Viviane <vivi29emi@gmail.com>
2022-05-12 11:01:25 -0300

    Adicionado a classe Repository

--- src/main/java/com/generation/graphome/repository/UsuarioRepository.java
+++ src/main/java/com/generation/graphome/repository/UsuarioRepository.java
@@ -2,10 +2,11 @@ package com.generation.graphome.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.generation.graphome.model.Usuario;
 
-public interface UsuarioRepository {
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
 
-	
-
+	public Optional<Usuario> findByUsuario(String usuario);
 }
