c36722d80a8e75ffd91614e46c8272671c930522
Patryck Silva <patrycksilva70@gmail.com>
2022-05-12 15:24:40 -0300

    Consertando o retorno do objeto no métodod de autenticar usuário

--- src/main/java/com/generation/graphome/service/UsuarioService.java
+++ src/main/java/com/generation/graphome/service/UsuarioService.java
@@ -20,70 +20,89 @@ import com.generation.graphome.repository.UsuarioRepository;
 public class UsuarioService {
 
 	@Autowired
-	private UsuarioRepository usuarioRepository;
+	private UsuarioRepository usuariorepository;
 	
-	public Optional <Usuario> cadastrarUsuario(Usuario usuario){
-		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario){
+		if(usuariorepository.findByUsuario(usuario.getUsuario()).isPresent())
 			return Optional.empty();
-		if (calcularIdade(usuario.getDataNascimento())<18)
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Sua idade é menor que 18",null);
-		usuario.setSenha(criptografarSenha(usuario.getSenha()));
-		return Optional.ofNullable(usuarioRepository.save(usuario));
-	}
-	
-	public Optional <Usuario> atualizarUsuario (Usuario usuario){
-		if (usuarioRepository.findById(usuario.getId()).isPresent()) {
-		Optional <Usuario>	buscaUsuario=usuarioRepository.findByUsuario(usuario.getUsuario());
-		if ((buscaUsuario.isPresent())&&(buscaUsuario.get().getId()!=usuario.getId()))
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Usuario já Existente",null);
+		
+		if(calculoIdade(usuario.getDataNascimento())<18)
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Usuário é menor de idade",null);
 		
 		usuario.setSenha(criptografarSenha(usuario.getSenha()));
-		return Optional.ofNullable(usuarioRepository.save(usuario));
+		
+		return Optional.of(usuariorepository.save(usuario));
+	}
+	
+	public Optional<Usuario> atualizarUsuario(Usuario usuario){
+		
+		if(usuariorepository.findById(usuario.getId()).isPresent()) {
 			
+			Optional<Usuario> buscaUsuario = usuariorepository.findByUsuario(usuario.getUsuario());
+			
+			if ( (buscaUsuario.isPresent()) && ( buscaUsuario.get().getId() != usuario.getId()))
+					throw new ResponseStatusException(
+							HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+			if(calculoIdade(usuario.getDataNascimento())<18)
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Usuário é menor de idade",null);
+			
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+			return Optional.ofNullable(usuariorepository.save(usuario));
 		}
 		return Optional.empty();
 	}
-	
-	public Optional <UsuarioLogin> autenticarUsuario (Optional<UsuarioLogin> usuarioLogin){
-		Optional <Usuario>	buscaUsuario=usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
-		if (buscaUsuario.isPresent()) {
-			if (compararSenha(usuarioLogin.get().getSenha(),buscaUsuario.get().getSenha())) {
+public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin){
+		
+		Optional<Usuario> buscaUsuario = usuariorepository.findByUsuario(usuarioLogin.get().getUsuario());
+		
+		if(buscaUsuario.isPresent()) {
+			
+			if( compararSenhas(usuarioLogin.get().getSenha(), buscaUsuario.get().getSenha()) ) {
+				
 				usuarioLogin.get().setId(buscaUsuario.get().getId());
 				usuarioLogin.get().setNome(buscaUsuario.get().getNome());
+				usuarioLogin.get().setBio(buscaUsuario.get().getBio());
+				usuarioLogin.get().setFoto(buscaUsuario.get().getBio());
 				usuarioLogin.get().setDataNascimento(buscaUsuario.get().getDataNascimento());
-				usuarioLogin.get().setToken(gerarBasicToken(usuarioLogin.get().getUsuario(),usuarioLogin.get().getSenha()));
+				usuarioLogin.get().setTipo(buscaUsuario.get().getTipo());
+				usuarioLogin.get().setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
 				usuarioLogin.get().setSenha(buscaUsuario.get().getSenha());
 				
 				return usuarioLogin;
 			}
-	}
-	
-				return Optional.empty();
+			
+		}
 		
+		return Optional.empty();
+	
 	}
-	
-// Metodos auxiliares
-	
-	private boolean compararSenha(String senhaDigitada, String senhaBanco) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder.matches(senhaDigitada, senhaBanco);
-		
-	}
-	
-	private String gerarBasicToken (String usuario, String senha) {
-		String token=usuario+":"+senha;
-		byte [] tokenBase64=Base64.encodeBase64(token.getBytes(Charset.forName("US-ASCII")));
-		return "Basic"+new String (tokenBase64);
-		
-	}
-	
-	
+//metodos complementares
 	private String criptografarSenha(String senha) {
+
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
 		return encoder.encode(senha);
 	}
+	
+	private boolean compararSenhas(String senhaDigitada, String senhaBanco) {
 
-	private int calcularIdade(LocalDate dataNascimento) {
-		return Period.between(dataNascimento, LocalDate.now()).getYears();
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		return encoder.matches(senhaDigitada, senhaBanco);
+	}
+	
+	private String gerarBasicToken(String usuario, String senha) {
+		
+		String token = usuario + ":" + senha;
+		byte[] tokenBase64 = Base64.encodeBase64(token.getBytes(Charset.forName("US-ASCII"))); 
+		
+		return "Basic " + new String(tokenBase64);
+	
+	}
+	
+	private	int calculoIdade(LocalDate nascimento) {
+		
+		return Period.between(nascimento,LocalDate.now()).getYears();
 	}
 }
